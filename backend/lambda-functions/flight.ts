ea43b54c6d90c88fb4a60c118951f59f47fb6a08
huukhang1512 <huukhang1512@gmail.com>
2022-03-26 20:54:55 +1100

    add template for lambda function

--- backend/lambda-functions/flight.ts
+++
@@ -1,9 +0,0 @@
-const getFlights = async(event: any) => {
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "application/json" },
-        body: `Hello, CDK! You've hit ${event.path}\n`
-    };
-};
-
-module.exports = {getFlights};
--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,15 +1,18 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
 
 export class BackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
-      code: lambda.Code.fromAsset('lambda-functions') ,  // code loaded from "lambda" directory
-      handler: 'flights.handler'                // file is "hello", function is "handler"
+
+    const getListOfFlightBooking = new NodejsFunction(this,
+      'GetListOfFlightBooking', {
+        runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
+        handler: 'getFlights',
+        entry: path.join(__dirname, `/../src/lambda-functions/flight.ts`)
     });
   }
 }
